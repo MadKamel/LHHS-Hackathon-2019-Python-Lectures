40e69c2b205804aac94ead4393e7e68d1cf3d9c4
Eric Rustrum <rustrumek@gmail.com>
2020-10-30 12:48:59 -0700

    Create main.py
    I really never use float() in any of my programs. This might be the only time I've ever used it, in fact!

---
+++ Program 4 (Final)/main.py
@@ -0,0 +1,12 @@
+x = float(input("Enter your first number: "))
+y = float(input("Enter your second number: "))
+o = input("Enter an operator: ")
+
+if(o=="+" or o=="add"):
+  print(x+y)
+if(o=="-" or o=="subtract"):
+  print(x-y)
+if(o=="*" or o=="multiply"):
+  print(x*y)
+if(o=="/" or o=="divide"):
+  print(x/y)
